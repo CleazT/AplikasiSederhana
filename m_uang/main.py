5bbb4d5359c4ac41ba2b6dabbff11a0bacd8d305
Dzaki <dzakigunawan34@gmail.com>
2024-02-19 08:42:04 +0700

    1.0.1

--- m_uang/main.py
+++ m_uang/main.py
@@ -5,9 +5,9 @@ def konv():
     pilih = input("Pilih Konversi [USD -> IDR, IDR -> USD]\n")
     if pilih == "USD -> IDR":
         num1 = input("Masukan Jumlah Uang Dollar:\n")
-        print(f'{int(num1)} USD di konversikan menjadi IDR adalah Rp {round(int(num1) * 15580, 2)},00\n')
+        print(f'{float(num1)} USD di konversikan menjadi IDR adalah Rp {(float(num1) * 15580, 2)},00\n')
     elif pilih == "IDR -> USD":
         num1 = input("Masukan Jumlah Uang Rupiah:\n")
-        print(f'{int(num1)} IDR di konversikan menjadi USD adalah {round(int(num1) / 15580, 2)}$\n')
+        print(f'{float(num1)} IDR di konversikan menjadi USD adalah {(float(num1) / 15580, 2)}$\n')
     else:
         print("Pilihan yang anda input tidak exist.")
